cdf3eaeec90a66519b7b8739ba44d8579881ff14
Remus Joseph Victuelles <rvictuelles@rvictuell-ltm.internal.salesforce.com>
2021-06-09 00:46:27 +1000

    Initial Commit

--- config/plugins.js
+++ config/plugins.js
@@ -2,9 +2,9 @@ module.exports = ({ env }) => ({
    upload: {
       provider: 'cloudinary',
       providerOptions: {
-         cloud_name: env('CLOUDINARY_NAME'),
-         api_key: env('CLOUDINARY_KEY'),
-         api_secret: env('CLOUDINARY_SECRET'),
+         cloud_name: env('CLOUDINARY_NAME', 'djkzcwdvg'),
+         api_key: env('CLOUDINARY_KEY', '978362226944714'),
+         api_secret: env('CLOUDINARY_SECRET', '5eMINU6elflQK3ARn7VquRnUXhM'),
       },
       actionOptions: {
          upload: {},
